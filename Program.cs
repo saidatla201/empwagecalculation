acaff34f1309ec6cf6aa2d9f459469a10c62a8cd
saidatla201 <saidatla201@gmail.com>
2023-01-03 07:23:09 +0530

    Maximum working hours is 100

--- Program.cs
+++ Program.cs
@@ -15,12 +15,14 @@ namespace empwagecalculation
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
-            int empHrs = 0, empWage = 0, totalWage = 0, day = 1;
+            const int Max_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalhrs = 0;
 
             //UC-1 Attendance
             Random random = new Random();
-            //UC-5
-            for (day = 1; day <= MAX_WORKING_DAYS; day++)
+            //UC-6
+            while ( day <= MAX_WORKING_DAYS && totalhrs <= Max_WORKING_HRS  )
+
             {
                 int employeeInput = random.Next(0, 3);
                 //UC-4- Switch case
@@ -38,9 +40,12 @@ namespace empwagecalculation
                 }
                 //UC-2 dailywise calculation
                 empWage = EMP_RATE_PER_HR * empHrs;
-                totalWage += empWage; 
+                totalWage += empWage;
+                totalhrs += empHrs;
+                day++;
             }
-            Console.WriteLine("Total wage for {0} days:{1}", (day - 1), totalWage);
+            Console.WriteLine("Total wage for {0} days:{1} and Hrs: {2}", (day - 1), totalWage,(totalhrs -empHrs ));
+            Console.ReadLine();
         }
     } 
 }
